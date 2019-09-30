d4ee1acfb7ebb90480c7e26f5ddb91075adbe41a
Aaron todd <altodd@full-165-91-48-100.vpn.tamu.edu>
2019-09-29 21:52:25 -0500

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort_by]))
-    @sort_column = params[:sort_by]
+    @movies = Movie.order(params[:sort_by])
+    @sort = params[:sort_by]
   end
 
   def new
